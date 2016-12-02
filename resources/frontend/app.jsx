c1e2c0251a0e75cb83089433ae6f2d3d4cb12848
Tamara <merrick1988@gmail.com>
2016-12-02 12:07:12 +0200

    add typescript and @angular

--- resources/frontend/app.jsx
+++
@@ -1,9 +0,0 @@
-require.context("./images", true);
-
-import 'jquery';
-import 'lodash';
-import './styles/styles.scss';
-
-window.console = window.console || {error() {}, log() {}, debug() {}};
-
-_.noConflict();
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,20 @@
-const webpack = require("webpack"),
-    path = require("path"),
-    ExtractTextPlugin = require("extract-text-webpack-plugin"),
+const webpack = require('webpack'),
+    path = require('path'),
+    ExtractTextPlugin = require('extract-text-webpack-plugin'),
     appPath = __dirname,
-    frontendPath = path.join(appPath, "resources/frontend"),
-    distPath = path.join(appPath, "public/static");
+    frontendPath = path.join(appPath, 'resources/frontend'),
+    distPath = path.join(appPath, 'public/static');
 
 module.exports = {
     cache: true,
     entry: {
-        index: path.join(frontendPath, 'app.jsx')
+        polyfills: path.join(frontendPath, 'polyfills.ts'),
+        app: path.join(frontendPath, 'app.ts'),
+        vendor: path.join(frontendPath, 'vendor.ts')
     },
     resolve: {
         root: frontendPath,
-        extensions: ['', '.jsx', '.js']
+        extensions: ['', '.ts', '.js']
     },
     output: {
         path: distPath,
@@ -21,35 +23,27 @@ module.exports = {
         library: '[name]'
     },
     module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: [
-                    'react-hot',
-                    'babel',
-                    'eslint'
-                ].join('!')
-            },
-
-            {
-                test: /\.scss$/,
-                loader: ExtractTextPlugin.extract(
-                    "style",
-                    "css!sass")
-            },
-            {
-                test: /\.(png|jpg|jpeg|svg)$/,
-                loader: 'url-loader?limit=8192&name=images/[name].[ext]'
-            },
-            {
-                test: /fonts\/.*\.(eot|ttf|woff|svg|svgz)$/,
-                loader: 'file?name=fonts/[name].[ext]'
-            }
-        ]
+        loaders: [{
+            test: /\.scss$/,
+            loader: ExtractTextPlugin.extract('style', 'css!sass')
+        }, {
+            test: /\.ts$/,
+            loaders: ['awesome-typescript-loader', 'angular2-template-loader']
+        }, {
+            test: /\.html$/,
+            loader: 'html'
+        }, {
+            test: /\.(png|jpg|jpeg|svg)$/,
+            loader: 'url-loader?limit=8192&name=images/[name].[ext]'
+        }, {
+            test: /fonts\/.*\.(eot|ttf|woff|svg|svgz)$/,
+            loader: 'file?name=fonts/[name].[ext]'
+        }]
     },
     plugins: [
-        new ExtractTextPlugin("app.css")
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ['app', 'vendor', 'polyfills']
+        })
     ],
     devServer: {
         noInfo: false,
